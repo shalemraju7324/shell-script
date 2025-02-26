d7e81162b7ecfc5e544a009d97be5f78004fe7bb
shalemraju <shalemrajudevops@gamil.com>
2025-02-26 17:15:40 +0530

    variables added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: hi i'm $PERSON2"
+echo "$PERSON2: Hello ! i'm $PERSON1"
+echo "$PERSON1 : How are you doing? "
+echo "$PERSON2: i'm good"
