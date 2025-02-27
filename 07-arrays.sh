62e3cb6d37769f9e56733b1295b77c5ca2e069ea
shalemraju <shalemrajudevops@gamil.com>
2025-02-27 15:54:28 +0530

    sum add values

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+MOVIES=("Pushapa" "RRR" "KGF")
+
+echo "First movie :${MOVIES[0]}"
+echo "SECOND movie :${MOVIES[1]}"
+echo "THIRD movie :${MOVIES[2]}"
+
+echo "All movies are : ${MOVIES[@]}"
