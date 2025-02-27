cafe764f888455558175cd5bfe3227fb28e3507a
shalemraju <shalemrajudevops@gamil.com>
2025-02-27 16:11:31 +0530

    sum add values

--- 08-speacial-var.sh
+++ 08-speacial-var.sh
@@ -3,7 +3,7 @@
 echo "All Variables passed:$@"
 echo "Number of variables:$#"
 echo "Script name : $0"
-echo "present working directory: $pwd"
+echo "present working directory: $PWD"
 echo "Home directory of current user :$HOME"
 echo "Which user is running this script :$USER"
 echo "process id of current script: $$"
