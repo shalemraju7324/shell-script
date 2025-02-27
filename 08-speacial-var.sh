604628682f56fd3e80209cbf57496b84c986838b
shalemraju <shalemrajudevops@gamil.com>
2025-02-27 16:08:33 +0530

    sum add values

---
+++ 08-speacial-var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+echo "All Variables passed:$@"
+echo "Number of variables:$#"
+echo "Script name : $0"
+echo "present working directory: $pwd"
+echo "Home directory of current user :$HOME"
+echo "Which user is running this script :$USER"
+echo "process id of current script: $$"
+echo "Process id of last command in background:$!" 
+sleep 60 &
+echo "Process id of last command in background:$!" 
