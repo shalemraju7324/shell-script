6e69625cb4b768aba0556839373e3d7580f8ac72
shalemraju <shalemrajudevops@gamil.com>
2025-02-28 20:01:39 +0530

    intallation check

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
-then 
-   echo "ERROR :: you must have sudo access to excute this script"
-  exit 1 #other than 0
-fi
-
-dnf list insalled mysql
-
-if [ $? -ne 0 ] # not installed
   then 
-      echo "installing MYSQL ... FAILURE"
-      exit 1
-else
-   echo " installing MYSQL ... SUCCESS"
+      echo "ERROR:: You must have sudo access to excute this script"
+      exit 1 #other than 0
 fi
 
-echo "MYSQL is already --- installed "
-
-# if [ $? -ne 0 ]
-# dnf install mysql -y
-
-# if [ $? -ne 0 ]
-#   then 
-#       echo "installing MYSQL ... FAILURE"
-#       exit 1
-# else
-#    echo " installing MYSQL ... SUCCESS"
-
-# fi
+dnf install mysql -y
 
 dnf install git -y
 
-if [ $? -ne 0 ]
-  then 
-      echo "installing GIT ... FAILURE"
-      exit 1
-else
-   echo " installing GIT9 ... SUCCESS"
-
-fi
