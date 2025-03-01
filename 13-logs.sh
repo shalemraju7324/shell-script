888badb80216e58370313ae0e4ac660f8cccc117
shalemraju <shalemrajudevops@gamil.com>
2025-03-01 16:59:14 +0530

    Functions testing

---
+++ 13-logs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/logs/shellscript.logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGSFOLDER/LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS"
+    fi
+}
+echo "Script started excuting at : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing MYSQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Insatalling GIT"
+else
+    echo -e "Git is already ... $Y INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
