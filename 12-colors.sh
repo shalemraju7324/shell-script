54842862f060835816f97487437d5b526aa27439
shalemraju <shalemrajudevops@gamil.com>
2025-03-01 16:29:11 +0530

    Functions testing

---
+++ 12-colors.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Insatalling GIT"
+else
+    echo "Git is already ... INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
