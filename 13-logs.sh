c65dc0f0da71da540b0e8ef397a9845f688cf1d2
shalemraju <shalemrajudevops@gamil.com>
2025-03-01 17:39:00 +0530

    Functions testing

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script.logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -13,10 +14,10 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R FAILURE"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ...$G SUCCESS"
+        echo -e "$2 ...$G SUCCESS $N"
     fi
 }
 echo "Script started excuting at : $TIMESTAMP" &>>$LOG_FILE_NAME
@@ -34,7 +35,7 @@ then # not installed
     dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 else
-    echo -e "MySQL is already ... $Y INSTALLED"
+    echo -e "MySQL is already ... $Y INSTALLED $N"
 fi
 
 dnf list installed git &>>$LOG_FILE_NAME
@@ -44,7 +45,7 @@ then
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Insatalling GIT"
 else
-    echo -e "Git is already ... $Y INSTALLED"
+    echo -e "Git is already ... $Y INSTALLED $N"
 fi
 
 
