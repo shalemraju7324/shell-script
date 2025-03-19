68eb702314e2084643063229f1c20a24adfec4be
shalemraju <shalemrajudevops@gamil.com>
2025-03-19 19:07:44 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -56,6 +56,8 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 if [ -n "$FILES" ] # true if there are files to zip
 then 
     echo "files are :: $FILES"
+    ZIP_FILE=$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "ZIP_FILE"
 else
     echo "No files older than $DAYS"
 fi
