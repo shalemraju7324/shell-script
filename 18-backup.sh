aa1129a1e4970f5f510cc0dccc6dca1fc1cbc09d
shalemraju <shalemrajudevops@gamil.com>
2025-03-19 18:58:27 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -53,7 +53,7 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ -n $FILES ]
+if [ -n "$FILES" ] # true if there are files to zip
 then 
     echo "files are :: $FILES"
 else
