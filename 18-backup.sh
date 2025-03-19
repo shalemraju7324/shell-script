6cbea6ac865b40f44cdb0abaa08652f9a7338ede
shalemraju <shalemrajudevops@gamil.com>
2025-03-19 19:24:57 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -15,15 +15,6 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
 USAGE(){
 
     echo -e "$R USAGE :: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optinal)>"
@@ -58,6 +49,17 @@ then
     echo "files are :: $FILES"
        ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "ZIP_FILE"
+    if[ -f "$ZIP_FILE"]
+    then
+    else
+        echo -e "sucessfully created zip files older than $DAYS"
+         while read -r filepath # here filepath is the variable name, you can give any name
+        do
+            echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+            rm -rf $filepath
+            echo "Deleted file: $filepath"
+        done <<< $FILES
+    else
 else
     echo "No files older than $DAYS"
 fi
